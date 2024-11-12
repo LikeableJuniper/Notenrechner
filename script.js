6859380cda05da4dfaa2dc39191551954e35d44e
LikeableJuniper <noa_steffen@hotmail.com>
2024-11-12 13:32:12 +0100

    Added point calculation and multiple input fields

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getAverage(returnValues=false) {
     let totalScore = 0;
     let totalWeight = 0;
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 1; i <= 14; i++) {
         let grade = Number(document.getElementById("grade" + i).value);
         let valueInputField = document.getElementById("weight" + i);
         let value = Number(valueInputField.value);
@@ -22,6 +22,29 @@ function getAverage(returnValues=false) {
     document.getElementById("averageOutput").innerHTML = "Durchschnitt: " + roundToThousands(totalScore/totalWeight);
 }
 
+function calculatePoints() {
+    let points = 0;
+    let grades = [];
+    for (let i = 1; i <= 14; i++) {
+        let grade = Number(document.getElementById("grade" + i).value);
+        //weights are irrelevant for calculating points, as all final averages should have a weight of 1 towards the point score
+        if (grade != 0) {
+            grades.push(grade)
+        }
+    }
+    for (let i = 0; i < 5; i++) {
+        let minimum = Math.min(...grades);
+        let index = grades.indexOf(minimum);
+        points += minimum;
+        grades.splice(index, 1);
+        if (grades.length == 0) {
+            break
+        }
+    }
+    let outputField = document.getElementById("pointsOutput");
+    outputField.innerHTML = "Punkte: " + points + "/19";
+}
+
 function roundToThousands(number) {
     return Math.round(number*1000)/1000;
 }
@@ -29,8 +52,8 @@ function roundToThousands(number) {
 function getNeeded() {
     getAverage();
     let [totalScore, totalWeight] = getAverage(returnValues=true);
-    let targetGrade = Number(document.getElementById("grade7").value);
-    let nextWeight = Number(document.getElementById("weight7").value);
+    let targetGrade = Number(document.getElementById("neededGrade").value);
+    let nextWeight = Number(document.getElementById("neededWeight").value);
     let requiredGrade = (targetGrade * (totalWeight+nextWeight) - totalScore)/nextWeight;
     document.getElementById("neededOutput").innerHTML = "Benötigte Note: " + roundToThousands(requiredGrade);
 }
